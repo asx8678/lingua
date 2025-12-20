3c3f65b14dc57bdfedaa870147f60c038fe42d29
Adam <asx8678@gmail.com>
2025-12-20 14:45:35 +0100

    Exclude redirect pages from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,23 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+const SITEMAP_EXCLUDE = new Set([
+  "/lekcje-angielskiego-online",
+  "/kurs-angielskiego-online",
+]);
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lingualegionowo.pl",
   output: "static",
   trailingSlash: "never",
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    sitemap({
+      filter: (page) => {
+        const { pathname } = new URL(page);
+        return !SITEMAP_EXCLUDE.has(pathname);
+      },
+    }),
+  ],
 });
