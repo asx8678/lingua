fc826fc1e26770bff4716ce30b5703e5e16db7e0
Adam <asx8678@gmail.com>
2025-12-20 11:23:32 +0100

    Add teacher photos and match card aspect ratio

--- src/data/teachers.ts
+++ src/data/teachers.ts
@@ -10,26 +10,31 @@ export const teachers: Teacher[] = [
     name: "Kasia",
     role: "Metodyk i Lektorka",
     languages: ["Angielski"],
+    image: "/images/teachers/kasia.png",
   },
   {
     name: "Ola",
     role: "Lektorka",
     languages: ["Angielski", "Hiszpański"],
+    image: "/images/teachers/ola.png",
   },
   {
     name: "Natalia",
     role: "Lektorka",
     languages: ["Hiszpański"],
+    image: "/images/teachers/natalia.png",
   },
   {
     name: "Janina",
     role: "Lektorka",
     languages: ["Rosyjski"],
+    image: "/images/teachers/janina.png",
   },
   {
     name: "Małgorzata",
     role: "Lektorka",
     languages: ["Niemiecki", "Szwedzki"],
+    image: "/images/teachers/malgorzata.png",
   },
   {
     name: "Marcin",
